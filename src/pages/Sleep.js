d7befaa0d8ea3c6b7e74f34b1aebec098af21b69
Dobherrmann <78302421+Dobherrmann@users.noreply.github.com>
2021-04-17 12:24:20 +0200

    updateSleep

--- src/pages/Sleep.js
+++ src/pages/Sleep.js
@@ -8,7 +8,7 @@ class Sleep extends Component {
     render() {
         return (
             <div>
-                Übernachtunen
+                Übernachtungen
             </div>
         );
     }
