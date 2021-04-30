21d5dc96e786e9bff2add707e57111bdbf7a7e54
Dobherrmann <78302421+Dobherrmann@users.noreply.github.com>
2021-04-30 22:56:44 +0200

    StylingÜbernachtungen

--- src/pages/Sleep.js
+++ src/pages/Sleep.js
@@ -11,30 +11,45 @@ class Sleep extends Component {
                 text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
                 link: "https://www.hotel-schloss-rabenstein.de/hotel/"
             },
+            {
+                hotel: "2",
+                text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+                link: "https://www.hotel-schloss-rabenstein.de/hotel/"
+            },
+            {
+                hotel: "3",
+                text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+                link: "https://www.hotel-schloss-rabenstein.de/hotel/"
+            },
+            {
+                hotel: "4",
+                text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+                link: "https://www.hotel-schloss-rabenstein.de/hotel/"
+            },
         ]
     }
 
     render() {
         return (
             <div className="sleep-container">
-                {this._createCard()}
-                {this._createCard()}
+                {this._createCard(this.state.lodgings[0])}
+                {this._createCard(this.state.lodgings[1])}
+                {this._createCard(this.state.lodgings[2])}
+                {this._createCard(this.state.lodgings[3])}
             </div>
         );
     };
 
 
-    _createCard() {
-        const link = <a href={this.state.lodgings[0].link} rel="noreferrer" target="_blank">Link {this.state.lodgings[0].hotel} <FiExternalLink /></a>;
+    _createCard(mylodings) {
+        const link = <a href={mylodings.link} rel="noreferrer" target="_blank">Link {mylodings.hotel} <FiExternalLink /></a>;
         return (
             <div className="sleep-card">
-                <h1 className="sleep-card-header">{this.state.lodgings[0].hotel}</h1>
-                <p className="sleep-card-text">{this.state.lodgings[0].text}</p>
+                <h1 className="sleep-card-header">{mylodings.hotel}</h1>
+                <div className=""></div>
+                <p className="sleep-card-text">{mylodings.text}</p>
                 <div className="sleep-card-link">{link}</div>
             </div>
-
-
-
         )
     }
 
